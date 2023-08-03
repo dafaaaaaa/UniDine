f8802874c699da9d286d4553d012eb47f155b50a
dafaaaaaa <daffataufik.2@gmail.com>
2023-08-03 23:14:28 +0700

    Login Form

--- dashmin-1.0.0/signin.php
+++ dashmin-1.0.0/signin.php
@@ -46,27 +46,22 @@
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
-                    <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                    <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3" style="box-shadow: 5px 10px 8px #888888;">
                         <div class="d-flex align-items-center justify-content-between mb-3">                        
-                            <h3>Sign In</h3>
+                            <h3>Login</h3>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <input type="email" class="form-control" id="floatingInput" placeholder="Username">
+                            <label for="floatingInput">Username</label>
                         </div>
                         <div class="form-floating mb-4">
                             <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                            <label for="floatingPassword">Password</label>                 
                         </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <a href="">Forgot Password</a>
+                        <div class="d-flex align-items-center justify-content-between mb-4">  
                         </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
+                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Login</button>
+                        
                     </div>
                 </div>
             </div>
